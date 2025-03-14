a6ecb7a502c7d5808012824b7d72ab459c6c3ae8
4ngelika <angelsitohang587@gmail.com>
2025-03-14 21:01:02 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+// tailwind.config.js
+module.exports = {
+    theme: {
+      extend: {
+        fontFamily: {
+          sans: ['Poppins', 'sans-serif'],
+        },
+        fontWeight: {
+          'font-sans1': '700',
+          'font-sans2': '300',
+        },
+      },
+    },
+    plugins: [],
+  };
